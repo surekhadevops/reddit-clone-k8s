67aa7a96eaa350211356d51634431d664c5dbe66
surekhadevops <158040741+surekhadevops@users.noreply.github.com>
2024-02-22 20:25:47 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "sushantops-reddit-clone" # Replace with your actual S3 bucket name
+    bucket = "sure-reddit-bucket" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
